2734d11d8e3c5bc2660c42f1ec7b83eef5ffa4d4
Oxters Wyzgowski <oxters168@gmail.com>
2020-02-26 00:29:26 +0200

    Got separate physics hand working, but still not satisfied. Started work on three part hand.

--- Assets/Core/Scripts/HandEmulator.cs
+++ Assets/Core/Scripts/HandEmulator.cs
@@ -1,71 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityHelpers;
 
-[RequireComponent(typeof(PhysicsTransform))]
 public class HandEmulator : MonoBehaviour
 {
     private readonly Quaternion wristFixupRotation = new Quaternion(0.0f, 1.0f, 0.0f, 0.0f);
 
     public Transform handAnchor;
 
-    public Transform[] bones;
-    public GameObject hiddenHand;
-    private PhysicsTransform[] bonePhysics;
+    public Transform meshRoot;
+    public Transform[] meshBones;
+    public PhysicsBone[] physicsBones;
+
+    //public GameObject hiddenHand;
 
     [SerializeField]
     private OVRSkeleton.IOVRSkeletonDataProvider _dataProvider;
-    private PhysicsTransform rootTransform;
 
     void Awake()
     {
         _dataProvider = GetComponent<OVRSkeleton.IOVRSkeletonDataProvider>();
-        rootTransform = GetComponent<PhysicsTransform>();
         //InitBones();
     }
 
     void Update()
     {
-        rootTransform.position = handAnchor.position;
-        rootTransform.rotation = handAnchor.rotation;
-
         if (_dataProvider != null)
         {
             var data = _dataProvider.GetSkeletonPoseData();
 
-            hiddenHand.SetActive(data.IsDataValid);
+            //hiddenHand.SetActive(data.IsDataValid);
             if (data.IsDataValid)
             {
-                for (var i = 0; i < bones.Length; ++i)
+                for (var i = 0; i < meshBones.Length; ++i)
                 {
-                    if (bones[i] != null)
+                    if (meshBones[i] != null && physicsBones[i] != null && physicsBones[i].bone != null && physicsBones[i].anchor != null)
                     {
-                        //bonePhysics[i].position = bonePhysics[i].transform.position;
-                        //bonePhysics[i].rotation = bonePhysics[i].transform.parent.rotation * data.BoneRotations[i].FromFlippedXQuatf();
-                        bones[i].localRotation = data.BoneRotations[i].FromFlippedXQuatf();
                         if (i == (int)OVRSkeleton.BoneId.Hand_WristRoot)
                         {
-                            bones[i].localRotation *= wristFixupRotation;
-                            //bonePhysics[i].rotation *= wristFixupRotation;
+                            physicsBones[i].bone.position = handAnchor.position;
+                            physicsBones[i].bone.rotation = handAnchor.rotation;
+                            physicsBones[i].bone.rotation *= wristFixupRotation;
+                            meshRoot.position = physicsBones[i].bone.transform.position;
+                            meshRoot.rotation = physicsBones[i].bone.transform.rotation;
+                        }
+                        else
+                        {
+                            physicsBones[i].bone.position = physicsBones[i].anchor.TransformPoint(meshBones[i].localPosition);
+                            physicsBones[i].bone.rotation = physicsBones[i].anchor.TransformRotation(data.BoneRotations[i].FromFlippedXQuatf());
+                            meshBones[i].rotation = physicsBones[i].bone.transform.rotation;
                         }
                     }
                 }
             }
         }
     }
+}
 
-    private void InitBones()
-    {
-        bonePhysics = new PhysicsTransform[bones.Length];
-        for (int i = 0; i < bones.Length; i++)
-        {
-            var physicsTransform = bones[i].GetComponent<PhysicsTransform>();
-            bonePhysics[i] = physicsTransform;
-
-            physicsTransform.striveForPosition = false;
-            physicsTransform.striveForOrientation = true;
-            physicsTransform.counteractGravity = true;
-        }
-    }
+[Serializable]
+public class PhysicsBone
+{
+    [SerializeField]
+    public Transform anchor;
+    [SerializeField]
+    public PhysicsTransform bone;
 }
