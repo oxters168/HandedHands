08fe3c09512c011d61c96930ab912254e5323013
Oxters Wyzgowski <oxters168@gmail.com>
2020-04-06 15:40:14 +0300

    Made the cube spawner move with joystick input in HandsTest. Made mirrored hands disappear/reappear when switch is on/off in HandsTest. Updated PhysicsGadgets. Updated VRPhysicsHands.

---
+++ Assets/Core/Scripts/MoveObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public PhysicsGadgets.Joystick joystick;
+    public Transform objectToMove;
+    public float speedMultiplier = 0.01f;
+
+    private void FixedUpdate()
+    {
+        objectToMove.position += new Vector3(joystick.horizontal, 0, joystick.vertical) * speedMultiplier;
+    }
+}
