48cc1e4ec86fceff1d64ffc7016994eac14a18a6
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-16 20:49:27 +0300

    Overhauled the hands and updated the test scene.

---
+++ Assets/Core/Scripts/DisplayAngle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DisplayAngle : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI label;
+    public PhysicsGadgets.InputWheel inputWheel;
+
+    void Update()
+    {
+        label.text = "Angle\n" + Mathf.RoundToInt(inputWheel.value * 360);
+    }
+}
---
+++ Assets/Core/Scripts/PalmToRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityHelpers;
+
+public class PalmToRotator : MonoBehaviour
+{
+    public VRPhysicsHands.HandEmulator hand;
+    public Rotator rotator;
+    public PhysicsGadgets.InputWheel wheelInput;
+
+    void Update()
+    {
+        if (hand != null)
+            rotator.spinAxis = hand.palm.forward;
+        rotator.angle = wheelInput.value * 360;
+    }
+}
